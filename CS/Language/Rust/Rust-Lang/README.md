64af6ead12f7657f83555b2255bb79ef6765ecfb
AshGreyG <ashgrey.huaier@gmail.com>
2025-01-16 22:43:54 +0800

    feat : add the rust-lang-2 and add some contents to the doc

--- CS/Language/Rust/Rust-Lang/README.md
+++ CS/Language/Rust/Rust-Lang/README.md
@@ -3,8 +3,9 @@
 This is my codes and notes collection of book [*THe Rust Programming Language*](https://doc.rust-lang.org/book), and there is my code contents:
 
 + 1 Getting Started
-   + 1.1 Installation
-   + 1.2 Hello, World!
-   + 1.3 Hello, Cargo!
-+ [2 Programming a Guessing Game](https://github.com/AshGreyG/To-Learn-By-Books/tree/main/CS/Language/Rust/Rust-Lang/rust-lang-1)
-+ 3 Common Programming Concepts
+  + 1.1 Installation
+  + 1.2 Hello, World!
+  + 1.3 Hello, Cargo!
++ [2 Programming a Guessing Game](./rust-lang-1/)
++ [3 Common Programming Concepts](./rust-lang-2/)
+  + 3.1 Variables and Mutability
---
+++ CS/Language/Rust/Rust-Lang/rust-lang-2/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let test = 5;
+    println!("The value of test is: {test}");
+
+    // x = 6;  // uncomment here
+    // ^^^^^
+    // cannot mutate immutable variable 'x'
+    //
+    // We can add 'mut' in front of the variable name to let
+    // the variable to be mutable
+
+    let mut test_mutable = 5;
+    println!("The value of test is: {test_mutable}");
+    test_mutable = 3;
+    println!("The value of test is: {test_mutable}");
+
+    const ONE_DAY_IN_SECONDS: u32 = 60 * 60 * 24;
+    println!("One day has {ONE_DAY_IN_SECONDS} seconds.");
+
+    // **constants** are values that are bound to a name and
+    // are not allowed to change. Constant globals cannot be
+    // mutable:
+    //
+    // const mut TEST_CONSTANT_MUTABLE: i32 = 3;   // uncomment here
+    //       ^^^
+    // const globals cannot be mutable
+
+    let test_shadow: i32 = 5;
+    let test_shadow = test_shadow + 1;
+    println!("Now the value of test_shadow is: {test_shadow}");
+
+    {
+        let test_shadow = test_shadow * 2;
+        println!("Now the value of inner scope test_shadow is: {test_shadow}");
+    }
+
+    println!("Now the value of test_shadow is: {test_shadow}");
+
+    // We can declare a new variable with the same name as a previous
+    // variable. Rustacean says that the first variable is shadowed
+    // by the second, which means the second variable is what the
+    // compiler will see when we use the name of the variable.
+
+    // By using 'let', we can perform a few transformations on a value
+    // but have the variable be immutable after those transformations
+    // have been completed.
+
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("Now 'spaces' is a number, its value is {spaces}");
+
+    // Using 'let' can transform the type of variable.
+
+}
