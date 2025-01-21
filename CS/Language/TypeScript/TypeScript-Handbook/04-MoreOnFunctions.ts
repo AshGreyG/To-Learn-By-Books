de2c966b85bb93cfd30f5808b3f9cbc1970e1b1e
AshGreyG <ashgrey.huaier@gmail.com>
2025-01-21 22:52:59 +0800

    feat : complete the 04-MoreOnFunctions.ts

--- CS/Language/TypeScript/TypeScript-Handbook/04-MoreOnFunctions.ts
+++ CS/Language/TypeScript/TypeScript-Handbook/04-MoreOnFunctions.ts
@@ -306,5 +306,164 @@ function testOverload(timestamp: number): Date;
 function testOverload(m: number, d: number, y: number): Date;
 function testOverload(mOrTimestamp: number, d?: number, y?: number): Date {
   if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+
+    // It matches the overload function
+    // function testOverload(m: number, d: number, y: number): Date;
+  } else {
+    return new Date(mOrTimestamp);
+
+    // It matches the overload function
+    // function testOverload(timestamp: number): Date;
   }
 }
+
+const date1 = testOverload(12345);
+const date2 = testOverload(2, 3, 4);
+// const date3 = testOverload(1, 3); // uncomment here
+//                            ^^^^
+// No overload expects 2 arguments, but overloads do exist that 
+// expect either 1 or 3 arguments
+
+// These first two signatures are called the **overload signature**.
+// Then we wrote a function implementation with a compatible signature.
+// Functions have an **implementation** signature, but this signature
+// can't be called directly. Even though we wrote a function with two
+// optional parameters after the required one, it can't be called with
+// two parameters.
+
+function testOverloadError1(x: string): void;
+function testOverloadError1() { }
+
+// testOverloadError1(); // uncomment here
+// ^^^^^^^^^^^^^^^^^^
+// Expected 1 arguments, but got 0.
+//
+// Again, the signature used to write the function body can't be seen
+// from the outside.
+
+// The signature of the implementation is not visible from the outside.
+// When writing an overloaded function, we should always have two or more
+// signatures above the implementation of the function.
+
+function testOverloadReturn(x: string): string;
+function testOverloadReturn(x: number): boolean;
+function testOverloadReturn(x : string | number): string | boolean {
+  if (typeof x === "string") {
+    console.log(x);
+    return "This is called by the '(string) => string' part";
+  } else {
+    return true;
+  }
+}
+
+function testOverloadError2(s: string): number;
+function testOverloadError2(arr: any[]): number;
+function testOverloadError2(x: any) {
+  return x.length;
+}
+
+testOverloadError2("This is a string");
+testOverloadError2([12, 2, 3, 1]);
+// testOverloadError2(Math.random() > 0.5 ? "Hello" : [1, 9]); // uncomment here
+//                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// No overload matches this call.
+// Overload 1 of 2, '(s: string): number', gave the following error.
+//   Argument of type 'number[] | "Hello"' is not assignable to parameter of type 'string'.
+//     Type 'number[]' is not assignable to type 'string'.
+// Overload 2 of 2, '(arr: any[]): number', gave the following error.
+//   Argument of type 'number[] | "Hello"' is not assignable to parameter of type 'any[]'.
+//     Type 'string' is not assignable to type 'any[]'
+
+// Because the type of argument we pass is 'number[] | string'
+
+function noReturn(): void {
+  return;
+}
+
+// 'void' represents the return value of functions which don't return a value.
+// It's the inferred type ant time a function doesn't have any 'return'
+// statements, or doesn't return any explicit value from those return
+// statements.
+//
+// In JavaScript, a function that doesn't return any value will implicitly
+// return the value 'undefined'. However, 'void' and 'undefined' are not
+// the same thing in TypeScript.
+
+// The special type 'object' refers to any value that isn't a primitive(
+// 'string', 'number', 'bigint', 'boolean', 'symbol', 'null' and 'undefined').
+// This is different from the **empty object** type {}, and also different
+// from the global type 'Object'.
+
+// In JavaScript, function values are objects: They have properties, have
+// 'Object.prototype' in their prototype chain, are 'instanceof Object',
+// we can call 'Object.keys' on them, and so on. For this reason, function
+// types are considered to be 'object's in TypeScript.
+
+function testAny(a: any) {
+  a.b();  // Ok
+}
+
+function testUnknown(a: unknown) {
+  // a.b();  // Not Ok, uncomment here
+  // ^
+  // 'a' is of type 'unknown'.
+}
+
+// The 'unknown' type represents 'any' value, but different to
+// 'any' type, but is safer because it's not legal to do anything
+// with an 'unknown' type.
+
+// This is useful when describing function types because we can describe
+// functions that accept any value without having 'any' values in our
+// function body.
+
+function safeParse(s: string): unknown {
+  return JSON.parse(s);
+}
+
+// Some functions never return a value. 'never' also appears when
+// TypeScript determines there's nothing left in a union.
+
+function fail(msg: string): never {
+  throw new Error(msg);
+}
+
+function testNever(x: string | number) {
+  if (typeof x === "string") {
+    // do something
+  } else if (typeof x === "number") {
+    // do something else
+  } else {
+    console.log(x); // let x: never
+  }
+}
+
+// In addition to using optional parameters or overloads to make functions
+// that can accept a variety of fixed arguments counts, we can also define
+// functions that take an **unbounded** number of arguments using
+// **rest parameters**.
+
+// A rest parameter appears after all other parameters, and use the '...'
+// syntax.
+
+function testRestParameter(n: number, ...m: number[]) {
+  return m.map((x) => n * x);
+}
+
+const testRestParameterResult = testRestParameter(10, 1, 2, 4);
+// 'testRestParameterResult' gets value [10, 20, 40]
+
+// Conversely, we can provide a variable number of arguments from an
+// iterable object (for example, an array) using the **spread** syntax.
+
+// Note that in general, TypeScript does not assume that arrays are immutable.
+// This can lead to some surprising behavior:
+
+const testSpread = [1, 2]; // let testSpread: number[]
+// const testResult1 = Math.atan2(...testSpread);  // uncomment here
+
+const testResult2 = [1].push(...testSpread);
+
+// Math.atan2(x: number, y: number): number
+// Array.push(...items: T[]): number
