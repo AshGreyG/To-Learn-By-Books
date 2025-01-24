f585eb823958e92bdcb5a1a53c718da6e9589017
AshGreyG <ashgrey.huaier@gmail.com>
2025-01-24 22:40:39 +0800

    feat : add CSS learning notes

--- CS/Tool/git/Learn-Git-Branching/09-Interactive-Rebase.sh
+++ CS/Tool/git/Learn-Git-Branching/09-Interactive-Rebase.sh
@@ -6,10 +6,19 @@ echo "git rebase -i HEAD~3"
 echo "git rebase bugFix main"
 
 # ============================
-# Juggling Commits
+# Juggling Commits #1
 # ============================
 
 echo "git rebase -i HEAD~2"     # order: C3 C2
 echo "git rebase -i HEAD~1"     # order: C2'
 echo "git rebase -i HEAD~2"     # order" C2'' C3'
 echo "git rebase caption main"  # 'caption' commits -> 'main' commits
+
+# ============================
+# Juggling Commits #2
+# ============================
+
+echo "git checkout main"
+echo "git cherry-pick C2"
+echo "git commit --amend"   # 'git commit --amend' is to amend the current commit
+echo "git cherry-pick C3"
---
+++ CS/Tool/git/Learn-Git-Branching/10-Git-Tag.sh
@@ -0,0 +1,6 @@
+# git tag <tag_name> <commit_id> is to add the tags whose name is
+# 'tag_name' to the commit <commit_id>
+
+echo "git tag v0 C1"
+echo "git tag v1 C2"
+echo "git checkout C2"
