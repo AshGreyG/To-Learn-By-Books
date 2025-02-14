170c4ae521fa2c9920971c91aa78c2c1facd6771
AshGreyG <ashgrey.huaier@gmail.com>
2025-02-14 22:38:09 +0800

    feat : add JavaScript notes

--- CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/01-Variables.js
+++ CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/01-Variables.js
@@ -30,3 +30,60 @@ let testRedeclarationsLet = 1;
 // let testRedeclarationsLet = 2;  // uncomment here
 //     ^^^^^^^^^^^^^^^^^^^^^
 // Cannot redeclare block-scoped variable 'testRedeclarationsLet'
+
+function testDeclarationBelowUse() {
+  phrase = "Hello";
+  alert(phrase);
+  var phrase;
+
+  // 'var' declaration are processed when the function starts or script
+  // starts for globals. In other words, 'var' variables are defined
+  // from the beginning of the function, no matter where the definition
+  // is. Assuming that the definition is not the nested function.
+
+  // People also call such behavior "hoisting" because all 'var' are hoisted
+  // to the top of the function.
+
+  neverPhrase = "never";
+  alert(neverPhrase);
+  if (false) {
+    var neverPhrase;
+  }
+
+  // Even as this, code blocks are ignored.
+}
+testDeclarationBelowUse();
+
+let testChange = 1;
+alert(testChange);  // 1
+testChange = 2;
+alert(testChange);  // 2
+
+// When the value is changed, the old data is removed from the variable
+
+// There are two limitations on variable names in JavaScript:
+// + The name must contain only letters, digits, or the symbols '$' and '_'
+// + The first character must not be a digit.
+// + Variables named 'apple' and 'APPLE' are two different variables.
+
+const myLoverBirthday = "10.07.2006";
+
+// To declare a constant / unchanging variable, use 'const' instead of 'let'
+
+// Task: Working with variables
+// ----------------------------
+
+var admin, name;
+name = "John";
+admin = name;
+alert(admin); // John
+
+// ----------------------------
+
+// Task: Giving the right name
+// ---------------------------
+
+let ourPlanetName = "Earth";
+let currentUserName;
+
+// ---------------------------
---
+++ CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/02-DataTypes.js
@@ -0,0 +1,9 @@
+// There are eight basic data types in JavaScript. We can put any type
+// in a variable. A variable can at one moment be a string and then
+// store a number. Programming languages that allow such things such
+// as JavaScript are called "dynamical typed", meaning that there exist
+// data types, but variables are not bound to any of them.
+
+let testMessage = "hello";  // testMessage: string
+testMessage = 12;           // testMessage: number now
+
