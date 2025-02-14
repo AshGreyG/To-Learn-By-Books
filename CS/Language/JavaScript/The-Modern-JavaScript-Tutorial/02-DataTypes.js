535ef35d5de1aafc4c56061134aa7c3260d15237
AshGreyG <ashgrey.huaier@gmail.com>
2025-02-14 23:18:34 +0800

    feat : add number type to The-Modern-JavaScript-Tutorial 02-DataTypes

--- CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/02-DataTypes.js
+++ CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/02-DataTypes.js
@@ -7,3 +7,39 @@
 let testMessage = "hello";  // testMessage: string
 testMessage = 12;           // testMessage: number now
 
+let numberIncludesInt = 123;        // numberIncludesInt: number
+let numberIncludesFloat = 123.123;  // numberIncludesFloat: number
+
+// The 'number' type represents both integer and floating point numbers.
+// Besides regular numbers, there are so-called "special numeric values"
+// which also belong to 'number':
+// + Infinity
+// + -Infinity
+// + NaN
+
+alert(1 / 0);     // Infinity
+alert(Infinity);  // Infinity
+
+// Infinity represents the mathematical Infinity, it's a special value 
+// that's greater than any number.
+
+alert("Not a number, it's a string" / 2); // NaN
+alert(NaN);                               // NaN
+
+// NaN represents a computational error. It's a result of an incorrect or
+// an undefined mathematical operation. NaN is sticky, any mathematical
+// operation on 'NaN' returns 'NaN'. There is only one exception:
+//
+// NaN ** 0 is 1
+
+alert(NaN + 1);   // NaN
+alert(NaN * 3);   // NaN
+alert(NaN ** 0);  // 1
+
+// In JavaScript, the 'number' type cannot safely represent integer values
+// larger than 2 ^ 53 - 1, that's 9007199254740991. The number type can store
+// larger integers up to 1.7976931348623157 * 10 ^ 308, but outside of the
+// safe integer range there'll be a precision error.
+
+alert(9007199254740991 + 1);
+alert(9007199254740991 + 2);
