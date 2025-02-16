f6b1cfb4fc7f2b93873deeab76ed708cb56ff371
AshGreyG <ashgrey.huaier@gmail.com>
2025-02-16 11:01:34 +0800

    feat : add null, undefined and typeof operator to 02-DataTypes

--- CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/02-DataTypes.js
+++ CS/Language/JavaScript/The-Modern-JavaScript-Tutorial/02-DataTypes.js
@@ -64,4 +64,61 @@ let testTemplateString = `${testDoubleQuotes} + is embedded`;
 // Backticks(``) are extended functionality quotes, they allow us to embed
 // variables and expressions into a string by wrapping them into '${...}'
 
-// 
+// The 'boolean' type has only two values: 'true' and 'false'. This type is
+// commonly used to store yes/no values
+
+let fieldCheckedName = false; // fieldCheckedName: boolean
+let fieldCheckedAge = true;   // fieldCheckedAge: boolean
+
+// The special value 'null' doesn't belong to any of the types described above.
+// It forms a separate type of its own which contains ony the 'null' value.
+// It's just a special value which represents nothing, empty or value unknown.
+
+let nullAge = null;
+
+// The special value 'undefined' also stands apart, it makes a type of its own,
+// just like 'null'. The meaning of 'undefined' is "value is not assigned".
+// If a variable is declared but not assigned, then its value is 'undefined'
+
+let undefinedAge;
+alert(undefinedAge);  // undefined
+
+undefinedAge = 3;
+undefinedAge = undefined;
+
+// Technically, it's possible to explicitly assign 'undefined' to a variable.
+// Normally, one uses 'null' to assign an empty or unknown value to a variable,
+// while 'undefined' is reserved as a default initial value for unassigned things.
+
+// All other types are called "primitive" because their values can contain only a
+// single thing. In contrast, 'objects' are used to store collections of data
+// and more complex entities.
+
+// The 'typeof' operator returns the type of the operand (string with the typename)
+
+alert(typeof undefined);    // undefined
+alert(typeof 0);            // number
+alert(typeof 123n);         // bigint
+alert(typeof true);         // boolean
+alert(typeof "fool");       // string
+alert(typeof Symbol("id")); // symbol
+alert(typeof Math);         // object
+alert(typeof null);         // object // wrong
+alert(typeof alert);        // function
+
+// 1. 'Math' is a built-in object that provides mathematical operations.
+// 2. The result of 'typeof null' is '"object"', that's an officially recognized
+//    error in 'typeof', coming from very early days of JavaScript and kept for
+//    compatibility. Definitely, 'null' is not an object.
+// 3. 'alert' is a function. Functions belong to the object type, but 'typeof'
+//    treats them differently, returning '"function"'
+
+// Task: String quotes
+// -------------------
+
+let name = "Ilya";
+alert(`hello ${1}`);      // hello 1
+alert(`hello ${"name"}`); // hello name
+alert(`hello ${name}`);   // hello Ilya
+
+// -------------------
