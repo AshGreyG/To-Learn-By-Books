a6ecc0586032dc4ae39644cc8e135215878ef2ef
AshGreyG <ashgrey.huaier@gmail.com>
2025-02-20 16:34:56 +0800

    feat : learn React Tutorial: Thinking in React

--- CS/Framework/React/react-doc/src/App.tsx
+++ CS/Framework/React/react-doc/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Game from './01-tutorial-tic-tac-toe.tsx';
+import FilterableProductTable from './02-thinking-in-react.tsx';
 
 function App() {
   return (
     <div className="react-doc">
       <Game />
+      <hr />
+      <FilterableProductTable />
     </div>
   );
 }
