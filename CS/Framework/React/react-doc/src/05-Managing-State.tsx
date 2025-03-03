77c5df7f71c2df15076b283105b7c2accf5da973
AshGreyG <ashgrey.huaier@gmail.com>
2025-03-03 23:40:13 +0800

    feat : learn React Tutorial: Manaing State

--- CS/Framework/React/react-doc/src/05-Managing-State.tsx
+++ CS/Framework/React/react-doc/src/05-Managing-State.tsx
@@ -1,6 +1,8 @@
 import React, { 
   useState,
-  useReducer
+  useReducer,
+  createContext,
+  useContext
 } from "react";
 
 function Form() {
@@ -261,7 +263,7 @@ function TaskAppUsingState() {
     { id: 1, text: "Watch a puppet show", done: false },
     { id: 2, text: "Lennon Wall pic",     done: false }
   ];
-  let nextID: number = 3;
+  let nextID: number = 2;
 
   const [tasks, setTasks] = useState<Task[]>(initialTasks);
 
@@ -305,8 +307,144 @@ function TaskAppUsingState() {
 //   presses "Save"
 // + 'handleDeleteTask(taskID)' is called when the user presses "Delete"
 
+type Reducer =
+  | { type: "added";   id: number; text: string; }
+  | { type: "changed"; task: Task; }
+  | { type: "deleted"; id: number; }
+
+function tasksReducer(tasks: Task[], action: Reducer): Task[] {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false
+        }
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+  }
+
+  // Reducer actually is named after the 'reduce()' operation that you can
+  // perform on arrays.
+
+  const arr: number[] = [1, 2, 3, 4, 5];
+  const sum: number = arr.reduce(
+    (result, num) => result + num
+  );
+
+  // The function you pass to 'reduce' is known as a reducer. It takes the
+  // result so far and the current item, then it returns the next result.
+  // React reducers are an example of the same idea: they take the state
+  // so far and the action, and return the next state.
+}
+
 function TaskAppUsingReducer() {
-  
+  const initialTasks: Task[] = [
+    { id: 0, text: "Visit Kafka Museum", done: true },
+    { id: 1, text: "Watch a puppet show", done: false },
+    { id: 2, text: "Lennon Wall pic", done: false }
+  ];
+  let nextID: number = 2;
+  const [tasks, dispatch] = useReducer<Task[], [action: Reducer]>(tasksReducer, initialTasks);
+
+  // The 'useReducer' hook is similar to 'useState', you must pass it an
+  // initial state and it returns a stateful value and a way to set state
+  // (and in this case, the 'dispatch' function.
+
+  function handleAddTask(text: string) {
+    dispatch({
+      type: "added",
+      id: nextID++,
+      text: text
+    });
+  }
+
+  function handleChangeTask(task: Task) {
+    dispatch({
+      type: "changed",
+      task: task
+    });
+  }
+
+  function handleDeleteTask(taskID: number) {
+    dispatch({
+      type: "deleted",
+      id: taskID
+    });
+  }
+}
+
+// 'useReducer' can help cut down on the code if many event handlers
+// modify state in a similar way. When updating states get more complex,
+// they can bloat your component's code and make it difficult to scan.
+// In this case, 'useReducer' lets you cleanly separate the how of update
+// logic from the what happened of event handlers.
+
+// Reducers must be pure. Similar to state updater functions, reducers run
+// during rendering. Actions are queued until the next render. This means
+// that reducers must be pure -- same inputs always result in the same output.
+// They should not send requests, schedule timeouts, or perform any side effects.
+// They should update objects and arrays state without mutations.
+
+// Usually, you will pass information from a parent component to a child component
+// via props. But passing props can become verbose and inconvenient if you have
+// to pass them through many components in the middle, or if many components in your
+// app need the same information. 'Context' lets the parent component make
+// some information available to any component in the tree below it, no matter
+// how deep, without passing it explicitly through props
+
+const LevelContext = createContext<number>(1);
+
+interface HeadingPropType {
+  children: React.ReactNode | undefined;
+}
+
+function Heading({ children }: HeadingPropType) {
+  const level: number = useContext<number>(LevelContext);
+
+  // 'useContext' tells React that the 'Heading' component wants to read
+  // the 'LevelContext'.
+
+  return (
+    <h1>{children}</h1>
+  )
+}
+
+interface SectionPropType {
+  level: number;
+  children: React.ReactNode | undefined;
+}
+
+function Section({ level, children }: SectionPropType) {
+  return (
+    <section className="section">
+      <LevelContext value={level}>
+        
+      </LevelContext>
+    </section>
+  );
+}
+
+function Page() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+    </Section>
+  )
 }
 
 function ManagingState() {
