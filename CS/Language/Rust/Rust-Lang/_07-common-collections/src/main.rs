133a68cb886dcec8baa51bcad07fea09db2ada33
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-05-09 22:36:49 +0800

    :bookmark: today (2025-05-09) learn the String and HashMap collection of Rust

--- CS/Language/Rust/Rust-Lang/_07-common-collections/src/main.rs
+++ CS/Language/Rust/Rust-Lang/_07-common-collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash, thread::Scope};
+
 fn main() {
     let mut test_vec: Vec<i32> = Vec::new();
 
@@ -84,5 +86,89 @@ fn main() {
     let s3 = s1 + &s2;
     println!("{}", s3); // Hello, world!
 
-    // Note that s1 has been moved here and can no longer be used.
+    // Note that s1 has been moved here and can no longer be used. The '+' operator uses
+    // the 'add' method, whose signature looks something like this:
+    //
+    // fn add(self, s: &str) -> String
+    //
+    // The reason we're able to use '&s2' in the call to 'add' is that the compiler can
+    // *coerce* the '&String' argument into a '&str'. When we call the 'add' method,
+    // Rust uses a *deref coercion*, which here turns '&s2' into '&s2[..]'.
+    //
+    // In the signature that 'add' takes ownership of 'self' because 'self' does not have
+    // an '&'. This means 's1' will be moved into the 'add' call and will no longer be
+    // valid after that.
+
+    let s4 = format!("{}-{}", s2, s3);  // format! macro returns the String with the contents.
+    println!("{}", s4);
+
+    // String doesn't support index to get the content. A 'String' is a wrapper over a 'Vec<u8>'.
+    // Some characters stored in String is not just a byte but 2 or 4 or even more bytes, if we use index
+    // to get them, they are not a valid character on its own.
+
+    let test_emoji = String::from("🐃");
+    println!("The length of String \"{}\" is {}", test_emoji, test_emoji.len());    // length = 4
+
+    // The best way to operate on pieces of strings is to bVe explicitly about whether
+    // you want characters or bytes.
+
+    println!("Output the chars of test_emoji");
+    for c in test_emoji.chars() { 
+        println!("{}", c);  // 🐃
+    }
+    println!("Output the bytes of test_emoji");
+    for b in test_emoji.bytes() {
+        println!("{}", b);  // 240 159 144 131
+    }
+
+    // --------- HashMap -------------
+
+    // The type 'HashMap<K, V>' stores a mapping of keys of type 'K' to values of type 'V'
+    // using a hashing function, which determines how it places these keys and values into
+    // memory
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // Like vectors, hash maps are homogeneous: all of the keys must have the same type, and
+    // all of the values must have the same type.
+
+    let blue_name = String::from("Blue");
+    let none_name = String::from("None");
+    let blue_score = scores.get(&blue_name).copied().unwrap_or(0);
+    let none_score = scores.get(&none_name).copied().unwrap_or(0);
+
+    println!("{} and {}", blue_score, none_score);  // 10 and 0
+
+    // The get method will return an 'Option<&V>'; if there's no value for that key in the
+    // hash map, 'get' will return 'None'. This program handles the 'Option' by calling
+    // 'copied' to get an 'Option<i32>' rather than an 'Option<&i32>', then 'unwrap_or'
+    // to set 'blue_score' to zero if 'scores' doesn't have an entry for the key.
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+
+        // We can iterate over each key-value pair in a hash map in a similar manner
+        // as we do with vectors.
+    }
+
+    // For types that implement the 'Copy' trait like 'i32', the values are copied into the
+    // hash map. For owned values like 'String' will be moved and the hash map will be the
+    // owner of those values.
+
+    // If we inset a key and a value into a hash map and then insert the same key with a
+    // different value, the value associated with that key will be overwritten.
+
+    scores.insert(String::from("Blue"), 20);
+    println!("{}", scores.get(&String::from("Blue")).copied().unwrap_or(0));    // 20
+
+    // Hash maps have a special API for checking the existence of a key: 'entry'. Its
+    // return value if an enum called 'Entry' that represents a value that might or
+    // might not exist.
+
+    scores.entry(String::from("Blue")).or_insert(50);
+    scores.entry(String::from("Red")).or_insert(50);
+    println!("{}", scores.get(&String::from("Blue")).copied().unwrap_or(0));    // 20
+    println!("{}", scores.get(&String::from("Red")).copied().unwrap_or(0));     // 50
 }
