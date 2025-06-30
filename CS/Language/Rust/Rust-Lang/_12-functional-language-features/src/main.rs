c46d4794fe623d33f4ae876d0e64a0dd12db71d9
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-06-30 23:14:24 +0800

    :sparkles: feat(rust-lang): update content of chapter 12 functional language features

---
+++ CS/Language/Rust/Rust-Lang/_12-functional-language-features/src/main.rs
@@ -0,0 +1,63 @@
+// Rust's closure are anonymous functions you can save in a variable or
+// pass as arguments to other functions.
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+
+        // The `unwrap_or_else` method on `Option<T>` is defined by the std.
+        // It takes one argument: a closure without any arguments that returns
+        // a value `T` (the same type stored in the `Some` variant of the
+        // `Option<T>`).
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+    // The user with preference Some(Red) gets Red
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+    // The user with preference None gets Blue
+}
