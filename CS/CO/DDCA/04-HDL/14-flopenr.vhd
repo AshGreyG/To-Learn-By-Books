02cf05403aad7a01627e2a35eb0f347c39b12332
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-01-18 20:33:03 +0800

    :desktop_computer: feat(computer-architecture): learn DDCA and implement flop with enable input and reset input

---
+++ CS/CO/DDCA/04-HDL/14-flopenr.vhd
@@ -0,0 +1,24 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+
+entity flopenr is
+  port(
+    clk,
+    reset, en: in  STD_LOGIC;
+    d:         in  STD_LOGIC_VECTOR(3 downto 0);
+    q:         out STD_LOGIC_VECTOR(3 downto 0)
+  );
+end;
+
+architecture asynchronous of flopenr is
+begin
+  process(clk, reset) begin
+    if reset then
+      q <= "0000";
+    elsif rising_edge(clk) then
+      if en then
+        q <= d;
+        -- q only copies the input d when en is true.
+      end if;
+    end if;
+  end process;
+end;
