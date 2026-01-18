a453fc24e655fe2331f4f5c857c1bb1e9608b4e0
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-01-18 18:29:00 +0800

    :desktop_computer: feat(computer-architecture): learn DDCA and implement resettable flip-clip (async and sync version)

---
+++ CS/CO/DDCA/04-HDL/13-flopr.vhd
@@ -0,0 +1,37 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+
+entity flop_resettable is
+  port(
+    clk, reset: in  STD_LOGIC;
+    d:          in  STD_LOGIC_VECTOR(3 downto 0);
+    q:          out STD_LOGIC_VECTOR(3 downto 0)
+  );
+end;
+
+architecture asynchronous of flop_resettable is
+begin
+  process(clk, reset) begin
+    -- Multiple signals in process sensitivity lists are the separated with a
+    -- comma. The name of these architectures are 'asynchronous' and 'synchronous'
+    -- is ignored by VHDL tools but may be helpful to the human reading the code.
+
+    if reset then
+      q <= "0000";
+    elsif rising_edge(clk) then
+      q <= d;
+
+    -- Notice 'elsif' but not 'elseif'
+    end if;
+  end process;
+end;
+
+architecture synchronous of flop_resettable is
+begin
+  process(clk) begin
+    if rising_edge(clk) then
+      if reset then q <= "0000";
+      else q <= d;
+      end if;
+    end if;
+  end process;
+end;
