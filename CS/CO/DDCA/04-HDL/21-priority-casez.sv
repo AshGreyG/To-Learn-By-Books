e091cec932e1e672780d607efabb586efdfe89ea
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-02-05 22:38:00 +0800

    :pencil2: typo: fix tab

--- CS/CO/DDCA/04-HDL/21-priority-casez.sv
+++ CS/CO/DDCA/04-HDL/21-priority-casez.sv
@@ -5,11 +5,11 @@ module priority_casez (
 
   always_comb
     casez (a)
-        4'b1???: y = 4'b1000;
-        4'b01??: y = 4'b0100;
-        4'b001?: y = 4'b0010;
-        4'b0001: y = 4'b0001;
-        default: y = 4'b0000;
+      4'b1???: y = 4'b1000;
+      4'b01??: y = 4'b0100;
+      4'b001?: y = 4'b0010;
+      4'b0001: y = 4'b0001;
+      default: y = 4'b0000;
     endcase
 
     // The 'casez' statement acts like a 'case' statement except that it also
